2c1e1a0fdd2ca3719d2212eaa6993bb5243f533e
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-13 23:00:23 +0530

    Created Unit test class with executable unit tests

---
+++ UnitTestClassLibrary/UnitTestControllers.cs
@@ -0,0 +1,35 @@
+﻿using Asp.netCoreMvcCrud.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+
+namespace UnitTestClassLibrary
+{
+    [TestFixture]
+    public class UnitTestControllers
+    {
+
+        [Test]
+        public void TestHomeIndex()
+        {
+            var obj = new HomeController();
+
+            var actResult = obj.Index() as ViewResult;
+
+            Assert.That(actResult.ViewName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public void TestPrivacyPage()
+        {
+            var obj = new HomeController();
+
+
+            var actResult = obj.Privacy() as ViewResult;
+
+            Assert.That(actResult.ViewName, Is.EqualTo("Privacy"));
+
+        }
+
+    }
+}
