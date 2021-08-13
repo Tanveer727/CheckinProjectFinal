5de95b5d6d0154302c5b7eaacefe8194a6c6d469
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-13 23:04:39 +0530

    Added Unit test for Integration test

---
+++ IntegrationTest/EmployeeControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using Asp.netCoreMvcCrud.Controllers;
+using Asp.netCoreMvcCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+
+namespace IntegrationTest
+{
+    [TestFixture]
+    public class EmployeeControllerTest
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeControllerTest(EmployeeContext context)
+        {
+            _context = context;
+        }
+        [Test]
+        public void TestDepartmentCreateRedirect()
+        {
+
+           
+                var obj = new HomeController();
+
+                var actResult = obj.Index() as ViewResult;
+
+                Assert.That(actResult.ViewName, Is.EqualTo("Index"));
+            
+
+
+
+        }
+    }
+}
