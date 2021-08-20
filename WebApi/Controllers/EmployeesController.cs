801ce0588b0c818b3ab093d637e31559702a4b45
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-20 22:50:44 +0530

    Added Employee API controller with all the functionality to fetch the Data from databse

---
+++ WebApi/Controllers/EmployeesController.cs
@@ -0,0 +1,64 @@
+﻿using Asp.netCoreMvcCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> Get()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public async Task Post([Bind("EmployeeId,FirstName,LastName,Department,EmpCode,Position,Email,OfficeLocation")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if (employee.EmployeeId == 0)
+                    _context.Add(employee);
+                else
+                    _context.Update(employee);
+                await _context.SaveChangesAsync();
+                 
+            }
+        }
+
+        
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int? id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            
+        }
+    }
+}
