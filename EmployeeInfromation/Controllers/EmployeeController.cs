eb43332d672abdafa71e6faf626e8ec1f820c9bd
Tanveer727 <79864037+Tanveer727@users.noreply.github.com>
2021-08-20 22:54:34 +0530

    Consuming the API to Perform CRUD function from my MVC Controller

--- EmployeeInfromation/Controllers/EmployeeController.cs
+++ EmployeeInfromation/Controllers/EmployeeController.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Asp.netCoreMvcCrud.Models;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +16,11 @@ namespace Asp.netCoreMvcCrud.Controllers
 {
     public class EmployeeController : Controller
     {
-        private readonly EmployeeContext _context;
+         
 
-        public EmployeeController(EmployeeContext context)
+        public EmployeeController()
         {
-            _context = context;
+            
         }
 
         // GET: /<controller>/
@@ -26,16 +32,81 @@ namespace Asp.netCoreMvcCrud.Controllers
         // GET: Employee
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Employees.ToListAsync());
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44348/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Employees");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var EmpResponse = result.Content.ReadAsStringAsync().Result;
+                    
+                    var readTask = JsonConvert.DeserializeObject<List<Employee>>(EmpResponse);
+
+
+                   
+                    return View(readTask);
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                   var  students = Enumerable.Empty<Employee>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                    return View(students);
+                }
+            }
+           // return View(students);
         }
+
+        //return View(await _context.Employees.ToListAsync());
+    
 
         // GET: Employee/Create
         public IActionResult AddorEdit(int id = 0)
         {
             if (id == 0)
+            {
                 return View(new Employee());
+            }
             else
-                return View(_context.Employees.Find(id));
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44348/api/");
+                    //HTTP GET
+                    var responseTask = client.GetAsync("Employees/"+id);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var EmpResponse = result.Content.ReadAsStringAsync().Result;
+
+                        var readTask = JsonConvert.DeserializeObject<Employee>(EmpResponse);
+
+
+
+                        return View(readTask);
+                    }
+                    else //web api sent error response 
+                    {
+                        //log response status here..
+
+                        var students = new Employee();
+
+                        ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                        return View(students);
+                    }
+                }
+
+                //return View(_context.Employees.Find(id));
+            }
         }
 
         // POST: Employee/Create
@@ -43,14 +114,37 @@ namespace Asp.netCoreMvcCrud.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddorEdit([Bind("EmployeeId,FirstName,LastName,Department,EmpCode,Position,Email,OfficeLocation")] Employee employee)
         {
+
+
             if (ModelState.IsValid)
             {
-                if (employee.EmployeeId == 0)
-                    _context.Add(employee);
-                else
-                    _context.Update(employee);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                //if (employee.EmployeeId == 0)
+                //    _context.Add(employee);
+                //else
+                //    _context.Update(employee);
+                //await _context.SaveChangesAsync();
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:44348/api/");
+
+                    //HTTP POST
+                    var postTask = client.PostAsJsonAsync<Employee>("Employees", employee);
+                    postTask.Wait();
+
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+
+                ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+                return View(employee);
+
+
+                //return RedirectToAction(nameof(Index));
             }
             return View(employee);
         }
@@ -58,10 +152,28 @@ namespace Asp.netCoreMvcCrud.Controllers
         // GET: Employee/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            var employee = await _context.Employees.FindAsync(id);
-            _context.Employees.Remove(employee);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44348/api/");
+
+                //HTTP DELETE
+                var deleteTask = client.DeleteAsync("Employees/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Index");
+            //var employee = await _context.Employees.FindAsync(id);
+            //_context.Employees.Remove(employee);
+            //await _context.SaveChangesAsync();
+            //return RedirectToAction(nameof(Index));
         }
     }
 }
